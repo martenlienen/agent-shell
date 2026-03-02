be00708ade589b8daefffcda71b8c6245c80ca56
xenodium <8107219+xenodium@users.noreply.github.com>
2026-03-02 09:47:55 +0000

    Bumping version

--- agent-shell.el
+++ agent-shell.el
@@ -4,10 +4,10 @@
 
 ;; Author: Alvaro Ramirez https://xenodium.com
 ;; URL: https://github.com/xenodium/agent-shell
-;; Version: 0.43.1
-;; Package-Requires: ((emacs "29.1") (shell-maker "0.85.1") (acp "0.11.1"))
+;; Version: 0.44.1
+;; Package-Requires: ((emacs "29.1") (shell-maker "0.86.1") (acp "0.11.1"))
 
-(defconst agent-shell--version "0.43.1")
+(defconst agent-shell--version "0.44.1")
 
 ;; This package is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -46,7 +46,7 @@
 (require 'json)
 (require 'map)
 (unless (require 'markdown-overlays nil 'noerror)
-  (error "Please update 'shell-maker' to v0.85.1 or newer"))
+  (error "Please update 'shell-maker' to v0.86.1 or newer"))
 (require 'agent-shell-anthropic)
 (require 'agent-shell-auggie)
 (require 'agent-shell-cline)
@@ -2191,8 +2191,8 @@ Set NO-FOCUS to start in background.
 Set NEW-SESSION to start a separate new session.
 SESSION-STRATEGY overrides `agent-shell-session-strategy' buffer-locally.
 OUTGOING-REQUEST-DECORATOR is passed through to `acp-make-client'."
-  (unless (version<= "0.85.1" shell-maker-version)
-    (error "Please update shell-maker to version 0.85.1 or newer"))
+  (unless (version<= "0.86.1" shell-maker-version)
+    (error "Please update shell-maker to version 0.86.1 or newer"))
   (unless (version<= "0.11.1" acp-package-version)
     (error "Please update acp.el to version 0.11.1 or newer"))
   (when (boundp 'agent-shell--transcript-file-path-function)
